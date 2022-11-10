e870619a61998056268696ed059bfbf49925ce5a
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2022-11-10 10:51:54 -0300

    scroll da tela mesmo sem ultrapassar o limite

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -16,41 +16,43 @@ class _loginPageState extends State<loginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextField(
-                  onChanged: (text) {
-                    print(text);
-                    email = text;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Email'
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    onChanged: (text) {
+                      print(text);
+                      email = text;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email'
+                    ),
                   ),
-                ),
-                SizedBox(height:15),
-                TextField(
-                  onChanged: (text) {
-                    print(text);
-                    senha = text;
-                  },
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Senha'
+                  SizedBox(height:15),
+                  TextField(
+                    onChanged: (text) {
+                      print(text);
+                      senha = text;
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Senha'
+                    ),
                   ),
-                ),
-                SizedBox(height:20),
-                TextButton(onPressed: (() {print('Email: $email\nSenha: $senha');}), child: Text('Entrar'))
-              ]),
+                  SizedBox(height:20),
+                  TextButton(onPressed: (() {print('Email: $email\nSenha: $senha');}), child: Text('Entrar'))
+                ]),
+          ),
         ),
       ),
     );
